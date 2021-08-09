449695800c0979d95470e1083d72cadcd6d36bb5
Omar Yakoubi <yakoubiomar2001@gmail.com>
2021-08-09 19:07:29 +0100

    ws-authentication

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(require("./routes"));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`SERVER STARTED ON PORT ${PORT}`);
-});
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -23,3 +17,9 @@ mongoose
   })
   .then(console.log("DATABASE CONNECTED ✅"))
   .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`SERVER STARTED ON PORT ${PORT}`);
+});
